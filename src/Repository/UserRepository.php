4b0c3db467df53350f80ddc52eaffe76e7e7bd00
petrero <petrero@gmail.com>
2018-03-11 12:52:02 +0200

    Copy start code from knpuniversityt site, hide nbproject folder and run in console 'composer install'

---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+class UserRepository extends ServiceEntityRepository{
+  public function __construct(ManagerRegistry $registry){
+    parent::__construct($registry, User::class);
+  }
+}
