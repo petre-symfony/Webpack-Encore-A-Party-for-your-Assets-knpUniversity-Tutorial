1b1974fb0c010f80716a18a9d959f01bd26ce1ec
petrero <petrero@gmail.com>
2018-03-13 19:51:47 +0200

    Now that Webpack is handling layout.js, let's simplify it! Remove the self-executing function.

--- public/assets/js/layout.js
+++ public/assets/js/layout.js
@@ -1,7 +1,7 @@
 'use strict';
 
-(function(window, $) {
-  $(document).ready(function() {
-    $('[data-toggle="tooltip"]').tooltip();
-  });
-})(window, jQuery);
+
+$(document).ready(function() {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
