45dc642cb22d4b645aa036c78bdbd31f05d1bf07
petrero <petrero@gmail.com>
2018-03-15 19:23:40 +0200

    Copy code from branch 7

--- public/assets/js/layout.js
+++ public/assets/js/layout.js
@@ -1,7 +1,14 @@
 'use strict';
 
-(function(window, $) {
-  $(document).ready(function() {
-    $('[data-toggle="tooltip"]').tooltip();
-  });
-})(window, jQuery);
+const $ = require('jquery');
+//window.jQuery = $;
+global.$ = $;
+require('bootstrap');
+require('bootstrap/dist/css/bootstrap.css');
+require('font-awesome/css/font-awesome.css');
+require('../css/main.css');
+
+$(document).ready(function() {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
