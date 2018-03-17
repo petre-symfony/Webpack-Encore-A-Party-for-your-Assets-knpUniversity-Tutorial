d586ced1656ee5f9354f7fbd8f67fbc5cc61604b
petrero <petrero@gmail.com>
2018-03-17 10:52:35 +0200

    Copy code from branch 12

---
+++ assets/js/layout.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import $ from 'jquery';
+//window.jQuery = $;
+//global.$ = $;
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
+import '../css/main.scss';
+
+$(document).ready(function() {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
